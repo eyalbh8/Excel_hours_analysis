d539676aa5f0c7089f010d5685294e3095055a4c
eyal ben haim <eyalbh8@gmail.com>
2021-11-01 17:57:10 +0200

    calculate hours function

--- functions.py
+++ functions.py
@@ -32,3 +32,43 @@ def meetingMinutes(meeting):
     calculateMinutes = int(meetings_EndsList[1]) - int(meetings_BeginList[1])
 
     return int(calculateMinutes)
+
+
+def CalculateHoursMintues(hours=0, minutes=0, totalHours=0, totalMinutes=0):
+
+    if minutes < 0 and hours == 0:
+        if minutes >= -30:
+            totalMinutes += -1 * minutes
+        else:
+            totalMinutes += 60 + minutes
+        
+    elif minutes < 0 and hours > 0:
+        if minutes >= -30:
+            totalMinutes += -1 * minutes
+        else:
+            totalMinutes += 60 + minutes
+        
+    elif hours < 0:
+        if minutes < 0:
+            if hours == -1:
+                if minutes >= -30:
+                    totalMinutes += -1 * minutes
+                else: 
+                    totalMinutes += 60 + minutes
+            else:
+                hours += (-1 * hours) - 1
+                if minutes >= -30:
+                    totalMinutes += -1 * minutes
+                else: 
+                    totalMinutes += 60 + minutes
+
+        else:
+            pass
+            
+    else:
+        totalHours = hours
+        totalMinutes += minutes
+
+    totalMinutes = totalMinutes/60
+    totalHours += totalMinutes
+    return totalHours
