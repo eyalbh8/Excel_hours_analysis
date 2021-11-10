9d8600c0706e74cdc7920421d13ed2dae6e23770
eyal ben haim <eyalbh8@gmail.com>
2021-11-10 16:47:19 +0200

    adding GUI

--- functions.py
+++ functions.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import tkinter as tk
 
 def readFile(file):
     df = pd.read_excel(file)
@@ -99,3 +100,73 @@ def DoctorHours(meetingTime, totalHours):
     
     else:
         return totalHours
+
+def Main(fileName, Ari_LastLine, Gilat_LastLine):
+
+    df = readFile(str(fileName))
+
+    meetingsList = Extracting_HoursData(df)
+
+    names = CustomersNames_List(df)
+
+    totalHours_Ari = 0
+    totalHours_Gilat = 0
+    totalHours_Emergency = 0
+
+    for meeting in meetingsList:
+        meetingTime = meeting[1][0]
+        meetingLine = int(meeting[0])
+
+        name = names[meetingLine][1][0]
+
+        if meetingLine < Ari_LastLine:
+       
+            if check_type(name):
+                totalHours_Ari = DoctorHours(meetingTime, totalHours_Ari)
+      
+            else:
+                pass
+    
+        elif Ari_LastLine < meetingLine < Gilat_LastLine:
+            if check_type(name):
+                totalHours_Gilat = DoctorHours(meetingTime, totalHours_Gilat)
+      
+            else:
+               pass
+
+        else:
+            if check_type(name):
+                totalHours_Emergency = DoctorHours(meetingTime, totalHours_Emergency)
+      
+            else:
+                pass
+
+    printResult(totalHours_Ari, totalHours_Gilat, totalHours_Emergency)
+
+def printResult(totalHours_Ari, totalHours_Gilat, totalHours_Emergency):
+    window = tk.Tk()
+    window.title("GO ILANA!!")
+
+    Result_label = tk.Label(window, text = "Result", font=('calibre',14, 'bold'))
+    Result_label.grid(row=0,column=1)
+
+    Ari_label = tk.Label(window, text = "Ari's hours:", font=('calibre',14, 'bold'))
+    Ari_label.grid(row=2,column=0)
+
+    AriResult_label = tk.Label(window, text = str(totalHours_Ari), font=('calibre',14, 'bold'))
+    AriResult_label.grid(row=2,column=2)
+
+    Gilat_label = tk.Label(window, text = "Gilat's hours:", font=('calibre',14, 'bold'))
+    Gilat_label.grid(row=3,column=0)
+
+    GilatResult_label = tk.Label(window, text = str(totalHours_Gilat), font=('calibre',14, 'bold'))
+    GilatResult_label.grid(row=3,column=2)
+
+    Emergency_label = tk.Label(window, text = "Emergency room hours:", font=('calibre',14, 'bold'))
+    Emergency_label.grid(row=4,column=0)
+
+    AriResult_label = tk.Label(window, text = str(totalHours_Emergency), font=('calibre',14, 'bold'))
+    AriResult_label.grid(row=4,column=2)
+
+    window.mainloop()
+
--- main.py
+++ main.py
@@ -1,46 +1,47 @@
-#01.20 Ari < 154 < Gilat < 400
-#02.20 Ari < 175 < Gilat < 430
-#03.20 Ari < 167 < Gilat < 436
-
+#01.21 Ari < 187 < Gilat < 419        #01.20 Ari < 154 < Gilat < 400
+#02.21 Ari < 161 < Gilat < 375        #02.20 Ari < 175 < Gilat < 430
+#03.21 Ari < 161 < Gilat < 428        #03.20 Ari < 167 < Gilat < 436
+#04.21 Ari < 169 < Gilat < 407        #04.20 Ari < 100 < Gilat < 360
+#05.21 Ari < 173 < Gilat < 419        #05.20 Ari < 154 < Gilat < 398
+#06.21 Ari < 154 < Gilat < 414        #06.20 Ari < 222 < Gilat < 517
+#07.21 Ari < 192 < Gilat < 450        #07.20 Ari < 189 < Gilat < 406
+#08.21 Ari < 188 < Gilat < 495        #08.20 Ari < 171 < Gilat < 393
+#09.21 Ari < 156 < Gilat < 383        #09.20 Ari < 162 < Gilat < 391
+#10.21 Ari < 214 < Gilat < 488        #10.20 Ari < 163 < Gilat < 371
+                                      #11.20 Ari < 213 < Gilat < 474
+                                      #12.20 Ari < 193 < Gilat < 406
+import tkinter as tk
 from functions import *
 
-df = readFile("1.20.xlsx")
+def Execute():
+   fileName = FileName.get()
+   Ari_LastLine = Ari_var.get()
+   Gilat_LastLine = Gilat_var.get()
+   Main(fileName, Ari_LastLine, Gilat_LastLine)
 
-meetingsList = Extracting_HoursData(df)
+window = tk.Tk()
+window.title("בס''ד")
 
-names = CustomersNames_List(df)
 
-totalHours_Ari = 0
-totalHours_Gilat = 0
-totalHours_Emergency = 0
+FileName = tk.StringVar()
+FileName_label = tk.Label(window, text = "File's name", font=('calibre',14, 'bold'))
+FileName_label.grid(row=0,column=1)
+FileName_entry = tk.Entry(window,textvariable = FileName, font=('calibre',14,'normal'))
+FileName_entry.grid(row=1,column=1)
 
-for meeting in meetingsList:
-   meetingTime = meeting[1][0]
-   meetingLine = int(meeting[0])
+Ari_var = tk.IntVar()
+Ari_label = tk.Label(window, text = "Ari's last line", font=('calibre',14, 'bold'))
+Ari_label.grid(row=2,column=1)
+Ari_entry = tk.Entry(window,textvariable = Ari_var, font=('calibre',14,'normal'))
+Ari_entry.grid(row=3,column=1)
 
-   name = names[meetingLine][1][0]
+Gilat_var = tk.IntVar()
+Gilat_label = tk.Label(window, text = "Gilat's last line", font=('calibre',14, 'bold'))
+Gilat_label.grid(row=4,column=1)
+Gilat_entry = tk.Entry(window,textvariable = Gilat_var, font=('calibre',14,'normal'))
+Gilat_entry.grid(row=5,column=1)
 
-   if meetingLine < 154:
-       
-      if check_type(name):
-         totalHours_Ari = DoctorHours(meetingTime, totalHours_Ari)
-      
-      else:
-         pass
-    
-   elif 154 < meetingLine < 400:
-      if check_type(name):
-         totalHours_Gilat = DoctorHours(meetingTime, totalHours_Gilat)
-      
-      else:
-         pass
+end = tk.Button(window,bg="black", fg="white",text="סיים",command=Execute)
+end.grid(row=6,column=1)
 
-   else:
-      if check_type(name):
-         totalHours_Emergency = DoctorHours(meetingTime, totalHours_Emergency)
-      
-      else:
-         pass
-
-print("Ari's hours {} \n Gilat's hours {} \n emergency hours {}"
-      .format(totalHours_Ari, totalHours_Gilat, totalHours_Emergency))
+window.mainloop()
