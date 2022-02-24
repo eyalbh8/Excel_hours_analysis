eac724f88902f06925960f7e91d93f07599249a5
eyal ben haim <eyalbh8@gmail.com>
2022-02-24 10:49:26 +0200

    test fixing

--- tests/test_hours_calculation.py
+++ tests/test_hours_calculation.py
@@ -4,4 +4,4 @@ from hours_calculating_functions import calculating_each_doctor_hours
 def test_calculating_total_hours():
     (ari, gilat, emergency) = calculating_each_doctor_hours('C:\Users\user\Desktop\Excel data analysis\examples\1.20.xlsx',
                                                           18, 22)
-    assert (ari, gilat, emergency) == (0.66666666666665, 81.00000000000001, 16.83333333333331)
+    assert (ari, gilat, emergency) == (60.66666666666665, 81.00000000000001, 16.83333333333331)
